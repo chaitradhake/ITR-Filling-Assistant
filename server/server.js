101c6c93785c72999448e63ef8427ee00c4d1a9d
Chaitra Dhake <chaitradhake21@gmail.com>
2026-07-12 20:41:55 +0530

    Added MongoDB Connection

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ import express from 'express';
 import dotenv from 'dotenv';
 import taxRouter from './routes/taxRoutes.js';
 import uploadRouter from './routes/uploadRoutes.js';
+import { connectDB } from './config/db.js';
 
 dotenv.config();
+
+// Connect to database
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
