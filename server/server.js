9679f8a6eb91e34858051fea5d6521a7217a2de0
Chaitra Dhake <chaitradhake21@gmail.com>
2026-07-15 02:28:58 +0530

    deployed frontend domain in CORS configuration

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import taxRouter from './routes/taxRoutes.js';
 import uploadRouter from './routes/uploadRoutes.js';
@@ -13,6 +14,10 @@ connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://itr-filling-assistant.vercel.app']
+}));
 
 app.use(express.json());
 app.use('/api', taxRouter);
